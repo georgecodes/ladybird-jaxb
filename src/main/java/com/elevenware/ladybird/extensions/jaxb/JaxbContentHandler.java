2176e16179ad4a40df564aff9c44b7ca97c8479a
George McIntosh <george.mcintosh@digital.cabinet-office.gov.uk>
2014-08-25 09:50:43 +0100

    merge

--- src/main/java/com/elevenware/ladybird/extensions/jaxb/JaxbContentHandler.java
+++ src/main/java/com/elevenware/ladybird/extensions/jaxb/JaxbContentHandler.java
@@ -1,6 +1,9 @@
 package com.elevenware.ladybird.extensions.jaxb;
 
 import com.elevenware.ladybird.entities.ContentHandler;
+import com.elevenware.ladybird.entities.ContentHandlers;
+
+import java.io.InputStream;
 
 public class JaxbContentHandler implements ContentHandler {
 
@@ -8,4 +11,18 @@ public class JaxbContentHandler implements ContentHandler {
         ContentHandlers.getInstance();
     }
 
+    @Override
+    public <T> T unmarshal(String payload, Class<T> clazz) {
+        return null;
+    }
+
+    @Override
+    public String marshal(Object payload) {
+        return null;
+    }
+
+    @Override
+    public <T> T unmarshal(InputStream inputStream, Class<T> clazz) {
+        return null;
+    }
 }
